71a80bba35f933a107a8c18b95891d271b68d49b
Christina Prokopenko <cmprokopenko@mun.ca>
2023-02-22 13:07:46 -0800

    More data exploration - time since investigation
    plotting daysEarly

---
+++ diet_RMNP.R
@@ -0,0 +1,22 @@
+
+#####processing All Sites and Scat input, calculating biomass, plots
+
+libs <- c('data.table', 'dplyr', 'tidyr', 'ggplot2', 'ggthemes', 'viridis', 'patchwork')
+lapply(libs, require, character.only = TRUE)
+
+
+####site data
+
+
+### scat data
+
+
+###merge sites and scat
+
+
+
+###biomass calculations
+
+
+
+####
--- explore_cluRMNP.R
+++ explore_cluRMNP.R
@@ -11,11 +11,11 @@ lapply(libs, require, character.only = TRUE)
 ####Load and subset data ####
 
 ### load All Sites data after cleaning protocol
-datrm <- fread('data/2021-09-17_RMNP_All_Sites.csv')
-
+datrm <- fread('data/2021-09-17_RMNP_All_Sites.csv')  ###6547 obs
+datrm <- datrm[Act_fixes <= 150] ###6433 obs. ##removes inv match only
 
 ### clusters matched with investigated sites
-inv_datrm <-subset(datrm, JoinStatus == 'Matched' & finalIgnore == 0)
+inv_datrm <-datrm[JoinStatus == 'Matched' & finalIgnore == 0]
 head(inv_datrm)
 
 ### unique investigated sites (remove multi wolves)
@@ -23,13 +23,17 @@ head(inv_datrm)
 
 ### clusters that are kills
 
-kclu_datrm <-subset(inv_datrm, Behaviour_1 == "Kill")
+kclu_datrm <- inv_datrm[Behaviour_1 == "Kill"]
 
 ### unique kill sites (remove multiple clusters and wolves) 
 
-ksite_datrm <- subset(kclu_datrm, mwKfirst == 1)
+ksite_datrm <- kclu_datrm[mwKfirst == 1]
 
 #### calculate mean actual fixes and radius for clusters
+
+### This is a tibble but I should change to data table
+
+summary(as.factor(datrm$JoinStatus))
 
 sum_clu <- summarise(group_by(datrm, JoinStatus), 
                    fix.mean=mean(Act_fixes), 
@@ -41,6 +45,41 @@ sum_clu <- summarise(group_by(datrm, JoinStatus),
 
 head(sum_clu)
 
+
+###calculate mean investigation time for sites
+
+### This is a tibble but I should change to data table
+
+summary(as.factor(inv_datrm$Behaviour_1))
+
+inv_datrm$Behaviour_1<-as.factor(inv_datrm$Behaviour_1)
+
+sum_inv <- summarise(group_by(inv_datrm, Behaviour_1), 
+                     days.mean=mean(daysEarly), 
+                     days.min = min(daysEarly),
+                     days.max=max(daysEarly))
+
+head(sum_inv) ###only has 6 behaviours which is wrong, should have more
+
+###investigations by size
+
+###these are tibbles but we should figure it out for data.table
+
+catfix <- datrm 
+catfix$Act_fixes <- as.factor(catfix$Act_fixes)
+catfix$JoinStatus <- as.factor(catfix$JoinStatus)
+
+fixfreq <- catfix%>% group_by(Act_fixes, JoinStatus) %>% summarize(Freq=n())
+
+clufreq <- subset(fixfreq, JoinStatus == 'CLUonly')
+invfreq <-subset(fixfreq, JoinStatus == 'Matched')
+
+allfreq <- left_join(invfreq,clufreq, by = 'Act_fixes') 
+propfix <- allfreq %>%  summarize(PropInv= (Freq.x/(Freq.x+Freq.y)))
+
+
+                   
+
 ####### all clusters #######
 ###plot size histogram for matched and not investigated clusters
 
@@ -48,28 +87,57 @@ p_clu <- ggplot(datrm) +
   geom_histogram(aes(x= Act_fixes, colour = JoinStatus), 
                  fill = "white", alpha = 0.2, position = "dodge") +
   geom_vline(data = sum_clu, aes(xintercept = fix.mean, colour = JoinStatus),
-             linetype="dashed")
-  #+ xlim(0,100)
+             linetype="dashed") + xlim(0,50)
 
 p_clu
+
+
+####proportion investigated by fixes
+
+p_invfix <- ggplot(propfix, aes(x = as.numeric(as.character(propfix$Act_fixes)), y = PropInv)) +
+  geom_point() + geom_smooth() + geom_hline(yintercept = .5, linetype = "dashed") + geom_vline( xintercept = 12, linetype = "dashed")
+
+p_invfix 
+
+
+### by cluster radius
 
 p_clu_rad <- ggplot(datrm) +
   geom_histogram(aes(x= Clus_rad_m, colour = JoinStatus), 
                  fill = "white", alpha = 0.2, position = "dodge") +
   geom_vline(data = sum_clu, aes(xintercept = rad.mean, colour = JoinStatus),
              linetype="dashed")
-#+ xlim(0,100)
+
 
 p_clu_rad
 
 
 
+
 ####### investigated clusters #######
 ####plot behaviour barplot for clusters
+########need to order by count
 
 p_beclu <- ggplot(inv_datrm) + geom_bar(aes(x = Behaviour_1))
 
 p_beclu
+
+####time by behaviour
+
+p_timeinv <- ggplot(inv_datrm,aes(x = reorder(Behaviour_1, -daysEarly), y = daysEarly)) 
+
+p_timeinv +  geom_boxplot() + coord_flip()
+
+ridge_timeinv <- 
+  ggplot(inv_datrm, aes(daysEarly, fct_rev(Behaviour_1), color = Behaviour_1, fill = Behaviour_1)) + 
+  coord_cartesian(clip = "off") +
+  scale_y_discrete(expand = c(.07, .07))  
+
+ridge_timeinv +   ggridges::stat_density_ridges(
+  quantile_lines = TRUE, quantiles = 2, 
+  color = "black", alpha = .8, size = 1) + xlim(-100,50)
+
+
 
 ##plot fix number by behaviour 
 
@@ -125,13 +193,13 @@ ridge_behavrad +
 
 ####### kill clusters #######
 ###plot prey histogram for unique kills by prey
-
+########need to order by count
 p_kclu <- ggplot(kclu_datrm) + geom_bar(aes(x = Prey_1))
 p_kclu
 
 
 ###plot prey histogram for unique kills by prey
-
+########need to order by count
 p_ksite <- ggplot(ksite_datrm) + geom_bar(aes(x = Prey_1))
 p_ksite
 
@@ -157,5 +225,4 @@ ridge_killrad <-
 ridge_killrad +   ggridges::stat_density_ridges(
   quantile_lines = TRUE, quantiles = 2, 
   color = "black", alpha = .8, size = 1) 
-
 
